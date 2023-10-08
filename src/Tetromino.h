17d69123a134d05816a011e800a048ae223b36c5
ManavBijlani <119971901+ManavBijlani@users.noreply.github.com>
2023-10-08 19:04:24 +1030

    Updated the Position and Tetromino header files

--- src/Tetromino.h
+++ src/Tetromino.h
@@ -4,17 +4,24 @@
 #include "Position.h"
 #include <map>
 #include <string>
+#include <vector>
+#include "Color.h"
+using namespace std;
 
 class Tetromino{
 
     private:
     int cell_size;
     int current_rotation;
+    std::vector<Color> colors;
 
 
 
     public:
+    int id;
     Tetromino();
+    std::map<int, std::vector<Position>> cells;
+
     void draw();
 };
 
