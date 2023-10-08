de967502584ba5dc7247eaf227bffff05e8886b9
ManavBijlani <119971901+ManavBijlani@users.noreply.github.com>
2023-10-08 19:05:40 +1030

    Defined a Draw Class
     The draw class stores the current_rotation state of tetromino block inside tiles vector and iterates through each one of them to draw a block
    using the raylib library

--- src/Tetromino.cpp
+++ src/Tetromino.cpp
@@ -1,9 +1,25 @@
-#include "Tetromino.h"
+#include"Tetromino.h"
+#include"Position.h"
+#include<raylib.h>
 
 //Default constructor for initializing all the attributes
 Tetromino::Tetromino(){
-    this -> cell_size=30;
-    this ->current_rotation=0;
-
+    this->cell_size=30;
+    this->current_rotation=0;
+    colors = Cell_Colours();
 };
 
+void Tetromino::draw() {
+//Storing the rotation of a tetromino in a vector
+    std::vector<Position> tiles = cells[current_rotation];
+
+//For loop to iterate through each element of the vector tiles
+    for (Position current_block: tiles) {
+
+        DrawRectangle(current_block.column * cell_size + 1, current_block.row * cell_size + 1, cell_size - 1,
+                      cell_size - 1,colors[id]);
+
+    };
+
+
+};
