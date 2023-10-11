00e661c5c95a3ff25cc075125fbacc39a1fcf8b4
Hussain Shakir <hussainshakir0421@gmail.com>
2023-10-11 19:17:55 +1030

    color files editted error exists fixed

--- src/Color.h
+++ src/Color.h
@@ -2,19 +2,6 @@
 #include "../vendor/raylib/src/raylib.h"
 #include <vector>
 
-
-//extern const Color yellow = YELLOW;
-//extern const Color orange = ORANGE;
-//extern const Color darkgreen = DARKGREEN;
-//extern const Color purple = PURPLE;
-//extern const Color skyblue = SKYBLUE;
-//extern const Color brown = BROWN;
-//extern const Color white = WHITE;
-//extern const Color red = RED;
-//extern const Color darkblue=DARKBLUE;
-//extern const Color pink=PINK;
-
-
 std::vector<Color> color_vector();
 
 
