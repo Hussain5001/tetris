e43df84e7c3abfd550e64847c8d992b0905b0d39
ManavBijlani <119971901+ManavBijlani@users.noreply.github.com>
2023-10-11 12:03:52 +1030

    Updated the set_initial_position()  function

--- src/Block_I.cpp
+++ src/Block_I.cpp
@@ -10,4 +10,17 @@ Block_I::Block_I(){
     cells[2] = {Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)};
     cells[3] = {Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)};
 }
+//Changing the initial position of the I block to make it start its movement from the beginning of the grid
+std::vector<Position> Tetromino::set_initial_position(){
 
+    std::vector<Position>tiles= cells[current_rotation];
+    std::vector<Position> movedTiles;
+
+    for (Position item:tiles){
+        Position new_pos= Position(item.row + -1, item.column + 3);
+        movedTiles.push_back(new_pos);
+    }
+
+    return movedTiles;
+
+}
--- src/Block_O.cpp
+++ src/Block_O.cpp
@@ -8,4 +8,18 @@ Block_O::Block_O(){
     cells[0] = {Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)};
 
 }
+//Changing the initial position of the O Block to move it's axis of rotation to the centre of the grid.
+std::vector<Position> Tetromino::set_initial_position(){
+
+    std::vector<Position>tiles= cells[current_rotation];
+    std::vector<Position> movedTiles;
+
+    for (Position item:tiles){
+        Position new_pos= Position(item.row + 0, item.column + 4);
+        movedTiles.push_back(new_pos);
+    }
+
+    return movedTiles;
+
+}
 
