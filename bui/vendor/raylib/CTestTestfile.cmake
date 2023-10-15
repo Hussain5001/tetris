be3a519b9acb5cd3516b9874fb6aedbc8fa1a743
Riddhi Agarwal <riddhi@MacBook-Air-3.local>
2023-10-15 13:14:07 +1030

    JSON added successfully

---
+++ bui/vendor/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/riddhi/uni/COMPSCI_1102/OOPproject-2023/vendor/raylib
+# Build directory: /Users/riddhi/uni/COMPSCI_1102/OOPproject-2023/bui/vendor/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
